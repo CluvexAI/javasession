e31146451ce4f54410ff83a73d749061a4d7f404
Tanmoy Mondal <160052620+CluvexAI@users.noreply.github.com>
2026-07-24 01:42:21 +0530

    Update welcome message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("welcome to java session by Tanmoy Mondal");
+        System.out.println("welcome to java session by Tanmoy Mondal. I am running CI with the help of Jenkins");
     }
 }
